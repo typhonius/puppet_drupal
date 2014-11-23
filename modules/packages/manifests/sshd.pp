f302a5c6ac48aa38b6edf52e239340fd94609753
Adam Malone <adam@adammalone.net>
2014-11-22 20:20:24 -0500

    fixed template path

--- modules/packages/manifests/sshd.pp
+++ modules/packages/manifests/sshd.pp
@@ -5,8 +5,7 @@ class packages::sshd {
   file { '/etc/ssh/sshd_config':
     ensure  => file,
     mode    => '0600',
-    # source  => 'puppet:///modules/services/sshd_config',
-    content => template('services/sshd_config.erb'),
+    content => template('packages/sshd_config.erb'),
   }
   service { 'sshd':
     ensure    => running,
